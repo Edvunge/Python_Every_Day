fc845279e7572554bba56fef099703f09acb7d7c
edvunge <costaedy18@gmail.com>
2024-01-02 21:15:02 +0000

    day2 - exercs

---
+++ Python_Every_Days/ExerciciosDiarios/exerc_day_02/exerc03.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+num = int(input("Digite um numero inteiro número: "))
+
+print("O numero digitado foi: ")
+print(num)
---
+++ Python_Every_Days/ExerciciosDiarios/exerc_day_02/exerc04.py
@@ -0,0 +1,13 @@
+
+
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+num1 = float(input("Digite o primeiro numero inteiro: "))
+num2 = float(input("Digite o segundo numero inteiro: "))
+num3 = float(input("Digite o terceiro numero inteiro: "))
+num4 = float(input("Digite o quarto numero inteiro: "))
+
+media = ((num1+num2+num3+num4)/4)
+
+print("A media digitada e: ")
+print(media)
