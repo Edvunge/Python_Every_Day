4c302c863376b12fddab3ca86b24aae6db3dcf03
edvunge <costaedy18@gmail.com>
2024-01-10 23:00:12 +0000

    day10 - day5 - for loops, range and code blocks

---
+++ Python_Every_Days/Master_Python_by_building_100_projects_in_100_days/Day_05/aula15_forLoopsRange.py
@@ -0,0 +1,12 @@
+#for loop with range
+#  for number in range(1, 11, 3):
+#       print(number)
+
+#For Loop with range
+#for number in range(1, 11, 3):
+#    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
