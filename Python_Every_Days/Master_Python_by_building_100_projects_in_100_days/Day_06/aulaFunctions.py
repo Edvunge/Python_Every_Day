29e9aaa1a9ba1b458804e9d3a67aba78ce8c2e8c
edvunge <costaedy18@gmail.com>
2024-01-11 21:51:28 +0000

    day11 - day6 - functions

---
+++ Python_Every_Days/Master_Python_by_building_100_projects_in_100_days/Day_06/aulaFunctions.py
@@ -0,0 +1,15 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# Defining Functions 
+
+# def my_function():
+#   Do this
+#   Then do this
+#   Finally do this
+
+# Calling Functions
+# my_function()
