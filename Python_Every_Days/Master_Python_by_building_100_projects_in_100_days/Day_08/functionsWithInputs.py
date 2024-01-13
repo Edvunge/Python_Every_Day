9be16ae1b7c58cd701017e14f2fb820f30d4dcdb
edvunge <costaedy18@gmail.com>
2024-01-13 23:47:32 +0000

    day13 - day8 - ceaser project

---
+++ Python_Every_Days/Master_Python_by_building_100_projects_in_100_days/Day_08/functionsWithInputs.py
@@ -0,0 +1,22 @@
+# Revisão: 
+# Crie uma função chamada greet(). 
+# Escreva 3 instruções de impressão dentro da função.
+# Chame a função greet() e execute o seu código.
+
+
+#simple Function
+#def greet():
+#    print("Hello Edvaldo.")
+#    print("How do you do jack Bauer?.")
+#    print("Ins't the weather nice today.")
+    
+#greet()
+
+#Function that allows for input
+
+def greet_with_name(name):
+    print(f'Hello {name}')
+    print(f'How do you do {name}')
+
+greet_with_name("edvaldo")
+
