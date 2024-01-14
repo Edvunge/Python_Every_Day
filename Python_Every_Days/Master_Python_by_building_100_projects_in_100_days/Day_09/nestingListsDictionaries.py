083aaa1c376fed3416ed524966f6e5fe9192eb10
edvunge <costaedy18@gmail.com>
2024-01-14 12:42:51 +0000

    day14 - day9 - dictionaries & nesting

---
+++ Python_Every_Days/Master_Python_by_building_100_projects_in_100_days/Day_09/nestingListsDictionaries.py
@@ -0,0 +1,11 @@
+#Nesting
+capitals = {
+    "France" : "Paris",
+    "Germany": "Berlin",
+}
+
+#Nesting a list in a Dictionary
+travel_log = {
+    "France": ["Paris","Lille","Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
