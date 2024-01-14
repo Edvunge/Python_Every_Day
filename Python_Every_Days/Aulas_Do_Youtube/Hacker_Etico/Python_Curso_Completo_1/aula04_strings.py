dfbfdac1ea976eb2c7f9c6a92dad99128bb67d2c
edvunge <costaedy18@gmail.com>
2024-01-14 23:49:25 +0000

    day14 - new courses

---
+++ Python_Every_Days/Aulas_Do_Youtube/Hacker_Etico/Python_Curso_Completo_1/aula04_strings.py
@@ -0,0 +1,42 @@
+######### STRINGS ###########
+print("ola")
+print('ola')
+
+w = "Curso de Python3"
+print(w)
+
+"""comentario"""
+
+a = """ola,
+este eh o curso de python
+espero que goste"""
+
+print(a)
+
+######### METODOS DE STRINGS ###########
+b = " ola Mundo "
+print(b.strip())
+
+# imprimir em letras minusculas
+c = "Ola Mundo"
+print(c.lower())
+
+# imprimir em letras maiusculas
+d = "Ola Mundo"
+print(d.upper())
+
+e = "ola"
+f = "Mundo"
+g = e + f
+
+print(g)
+
+a = "Ola Mundo"
+b = 5
+c = 3
+d = 1+2j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
