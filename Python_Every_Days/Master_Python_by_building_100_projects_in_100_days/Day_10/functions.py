d32c7c952808c3a8d123cafaeb67524742a20179
edvunge <costaedy18@gmail.com>
2024-01-15 21:25:10 +0000

    day15 - day10 - Functions with Outputs

---
+++ Python_Every_Days/Master_Python_by_building_100_projects_in_100_days/Day_10/functions.py
@@ -0,0 +1,9 @@
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    print(f'{formated_f_name}{formated_l_name}')
+
+formated_string = format_name("ed", "vung")
+print(format_name("ed", "vung"))
