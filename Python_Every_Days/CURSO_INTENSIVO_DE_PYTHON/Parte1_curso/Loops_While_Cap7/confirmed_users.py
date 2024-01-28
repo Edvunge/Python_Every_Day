68b4b78992cdb06b2a32c2786037cf8c851121fc
edvunge <costaedy18@gmail.com>
2024-01-28 22:30:06 +0000

    day28 - dicionario

---
+++ Python_Every_Days/CURSO_INTENSIVO_DE_PYTHON/Parte1_curso/Loops_While_Cap7/confirmed_users.py
@@ -0,0 +1,6 @@
+# Começa com os usuarios que precisam ser verificados,
+# e com uma lista vazia para armazenar os usuarios confirmados
+
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
