e0ea2747a313fddaac00c8a564ec988672bb0dc2
edvunge <costaedy18@gmail.com>
2024-01-29 23:46:02 +0000

    day29 - listas resumo

---
+++ Python_Every_Days/CURSO_INTENSIVO_DE_PYTHON/Parte1_curso/introducaoListas_Cap3/list_numero.py
@@ -0,0 +1,12 @@
+list = [1,2,3,4,5,6,7]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+
+# slicing the elements
+print(list[0:6])
+
+print(list[:])
+print(list[2:5])
+print(list[1:6:2])
