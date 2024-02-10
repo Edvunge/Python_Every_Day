a04880282b66d0dac41a30eeccca4f3b4fc6e60c
edvunge <costaedy18@gmail.com>
2024-02-10 21:02:28 +0000

    Weekend Project

---
+++ Python_Every_Days/ProjectosDeFinalDeSemana/Automating/codigo.py
@@ -0,0 +1,7 @@
+import wget
+
+link = "https://www.inf.ufpr.br/carmem/ci208/prova2-treino.pdf"
+wget.download(link)
+"""
+wget.download(link, "aqui ficara o nome do arquivo que eu quero guardar, com a extenssao (.pdf .doc .rtf) ")
+"""
