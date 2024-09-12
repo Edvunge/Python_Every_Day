7b0136dd89213bcdea17d9531a421009112bbca1
Edvunge <costaedy18@gmail.com>
2024-09-12 20:36:39 +0100

    update

---
+++ Python_Every_Days/Aulas/Adriana/aula01/TiposPrimitivos.py
@@ -0,0 +1,44 @@
+#print(5 + 8)
+
+#print(9 - 8)
+
+#print(2 * 3)
+
+#print(88 / 4)
+
+#print(7 ** 1)
+
+#print(7 % 1)
+
+#print((1 + 3) * 2 )
+
+# bool
+# true ( 5 + 5) 10
+# false ( 5 + 15) 10
+
+print(8 == 8)
+# 8 == 9 igualdade
+# r =  6 atribuiçao
+#  
+
+print(0 and 2)
+#  0 and 2 ( e )
+
+print(4 or 5)
+# 4 or 5 ( ou ) 
+
+
+print(1 == 1) #True
+print(1 == 12) #False
+
+print(2 + 7 == 1 + 8 == 5 * 2)
+
+print(1 != 21)
+
+print(1 < 21) 
+
+print(21 > 34)
+
+print(3 >= 2) #
+
+print(3 <= 2) #
---
+++ Python_Every_Days/Aulas/Adriana/aula01/Variaveis.py
@@ -0,0 +1,6 @@
+# Strings
+
+mensagem = "ola Adriana!"
+mensagem1 = "ola paulo!"
+#print(mensagem1) 
+print(mensagem + " " + mensagem1) #concatenação
