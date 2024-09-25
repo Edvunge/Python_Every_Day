b6f575011240c40e5a820a5dc1d134932a810b6a
edvaldo <costaedy18@gmail.com>
2024-09-25 16:19:45 +0100

    math_operators

---
+++ Python_Every_Days/Aulas_Do_Youtube/ClearCode/The_Complete_Guide_Python/Part_I_pythonBasics/first.py
@@ -0,0 +1,11 @@
+print("first line of code")
+print("second line of code")
+print(1234)
+
+print("")
+print("  X")
+print(" XXX")
+print("XXXXX")
+print("  X")
+print("  X")
+print("  X")
---
+++ Python_Every_Days/Aulas_Do_Youtube/ClearCode/The_Complete_Guide_Python/Part_I_pythonBasics/mathOperatos.py
@@ -0,0 +1,13 @@
+print(10 + 6)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+
+print(10 ** 6)
+print(7 // 2)
+print(7 % 2)
+
+print(5 + 5 * 2)
+
+print(10 > 5)
+print(10 < 6)
