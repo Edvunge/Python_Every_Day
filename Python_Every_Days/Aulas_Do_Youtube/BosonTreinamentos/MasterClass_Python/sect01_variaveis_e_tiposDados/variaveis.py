64fd37f56cc507dc44a0d921a0a57ce0eebd7e56
edvaldovunge <costaedy18@gmail.com>
2025-02-28 21:06:16 +0000

    variaveis e tipos de dados

---
+++ Python_Every_Days/Aulas_Do_Youtube/BosonTreinamentos/MasterClass_Python/sect01_variaveis_e_tiposDados/variaveis.py
@@ -0,0 +1,29 @@
+# String
+# nome_usuario = 'Edvaldo'
+
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome , idade = 'edvaldo', 47
+estado = True
+
+# Função type()
+# -----------------
+# print(type(media))
+# print(type(nome))
+# print(type(n2))
+# print(type(estado))
+# print(type(1+2j))
+
+
+# Funçao isinstance()
+# a = 10
+# b = 'sol'
+# print(isinstance(a, (int, float)))
+
+
+a = 40
+c = 3
+r = a * c
+print(r)
+
