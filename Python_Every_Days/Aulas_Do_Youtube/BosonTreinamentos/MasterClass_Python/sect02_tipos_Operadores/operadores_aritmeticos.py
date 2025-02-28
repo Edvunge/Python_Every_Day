673fc4f63f7fc21a81285df9dbec31093ddd97bb
edvaldovunge <costaedy18@gmail.com>
2025-02-28 21:32:30 +0000

    operadores aritmeticos, comparação

---
+++ Python_Every_Days/Aulas_Do_Youtube/BosonTreinamentos/MasterClass_Python/sect02_tipos_Operadores/operadores_aritmeticos.py
@@ -0,0 +1,11 @@
+a = 10
+b = 5
+c = 11
+
+print('ONDE a = 10, b = 5, c = 11')
+print('a + b = ' ,a + b)
+print('a - b = ', a - b)
+print('a * b = ', a * b)
+print('a / b = ', a / b)
+print('a % c = ', a % c)
+print('a ** b = ', a ** 2)
---
+++ Python_Every_Days/Aulas_Do_Youtube/BosonTreinamentos/MasterClass_Python/sect02_tipos_Operadores/operadores_de_comparacao.py
@@ -0,0 +1,12 @@
+x = y = z = False
+n1 = n2 = 0
+
+print("digite um numero: ")
+n1 = int(input())
+n2 = int(input("Digite outro numero: "))
+
+x = n1 == n2
+print("sao iguais? ", x, '\n')
+
+z = n1 > n2
+print(n1, " é maior que ", n2, "? ",z , "\n")
