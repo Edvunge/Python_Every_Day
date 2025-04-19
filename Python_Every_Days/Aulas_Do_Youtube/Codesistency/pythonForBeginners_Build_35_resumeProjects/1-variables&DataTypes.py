44cf7b87b0dc1c396ef812f8a9b516bfdeca426d
edvaldovunge <costaedy18@gmail.com>
2025-04-19 23:45:50 +0100

    basic operations in py

--- Python_Every_Days/Aulas_Do_Youtube/Codesistency/pythonForBeginners_Build_35_resumeProjects/1-variables&DataTypes.py
+++ Python_Every_Days/Aulas_Do_Youtube/Codesistency/pythonForBeginners_Build_35_resumeProjects/1-variables&DataTypes.py
@@ -1,1 +1,45 @@
 print("Hello World!")
+
+# Strings
+name = "Alex"
+
+# Integer (Whole Numbers)
+age = 25
+
+# Float (decimal number)
+height = 9.5
+
+# Boolean
+is_student = True
+
+print("Hey my name is " + name)
+print("Hey my name is ", name)
+
+print(name[-1])
+
+msg = "Hello world!"
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.replace("l", "L"))
+print("World" in msg)
+print(len(msg))
+
+
+greeting1 = "Hello"
+greeting2 = "hello"
+
+if greeting1 == greeting2:
+    print("They are the same")
+else:
+    print("They are different")
+
+
+# Type conversion
+age_str = "30"
+age_num = int(age_str)
+print(type(age_num))
+print(type(age_str))
+
+price_float = 29.99
+price_int = int(price_float)
---
+++ Python_Every_Days/Aulas_Do_Youtube/Codesistency/pythonForBeginners_Build_35_resumeProjects/2-basicOperations.py
@@ -0,0 +1,20 @@
+x = 10
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+# x = x + 15
+x += 15
+print(x)
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
